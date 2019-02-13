ebc6200c4d2d386528b99590686cb66e0b8fb980
Jestonls <jestonls@gmail.com>
2019-02-12 18:34:13 -0800

    Still have to add reset function

--- assets/crystal.js
+++ assets/crystal.js
@@ -1,49 +1,62 @@
-
 var randNum = [];
-for (var i = 45; i <= 101; i++) {
+for (var i = 25; i <= 101; i++) {
 randNum.push(i);
 }
 
 
-var guess = randNum[Math.floor(Math.random() * randNum.length)];    
-var div = document.getElementById('number-to-guess');
-div.innerHTML += guess;
-
-
-
-  var counter = 0;
-  
-  var div3 = document.getElementById('total');
-
-div3.innerHTML += counter;
-
-  var userGuess = [10, 5, 3, 7];
-
-$('#userguess1').text(userGuess[0]);
-$('#userguess2').text(userGuess[1]);
-$('#userguess3').text(userGuess[2]);
-$('#userguess4').text(userGuess[3]);
- 
-
-  
-
-  $(".crystal-image").on("click", function() {
-    for (var i = 0; i < userGuess.length; i++) {
-        var crystalValue = (userGuess[i]);
-        crystalValue = parseInt(crystalValue);
-    counter += crystalValue;
-    alert("New score: " + counter);
-    
-
-    if (counter === guess) {
-      alert("You win!");
-      $('#wins').text('YOU WIN!');
-    }
-
-    else if (counter >= guess) {
-      alert("You lose!!");
-      $('#loss').text('YOU\'RE DONE!');
-    }
+var randNumUser = [];
+for (var j = 1; j <= 10; j++) {
+randNumUser.push(j);
 }
 
-  });
+
+var guess = randNum[Math.floor(Math.random() * randNum.length)];
+var userGuess = randNumUser[Math.floor(Math.random() * randNumUser.length)];
+
+
+var div = document.getElementById('randNumber');
+
+div.innerHTML += guess;
+
+var crystal = document.getElementById('num');
+
+crystal.innerHTML += userGuess;
+
+var totals = document.getElementById('total');
+
+totals.innerHTML += sum();
+
+var start = false;
+var winnerLooser = false;
+var wins = 0;
+var loss = 0;
+
+function sum() {  
+;
+}
+
+
+
+function start(){
+start = true;
+$("#start").on("click", start);
+} 
+
+
+
+if (start === true) {
+
+
+  if (total === randNum) {
+    wins++;
+
+  } else if (total > randNum) {
+    loss++;
+  }
+
+
+$("#reset").on("click", reset);
+$("#start").on("click", start);
+
+}
+
